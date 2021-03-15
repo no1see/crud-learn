e997ae9a2f7690517e2130cbeedae1e73ab743d5
no1see <swegiy@gmail.com>
2021-03-15 19:58:12 +0200

    initital commit with default crud

---
+++ app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb://localhost:27017/noonesee_test_db"
+};
---
+++ app/routes/article.routes.js
@@ -0,0 +1,37 @@
+module.exports = app => {
+  const articles = require("../controllers/article.controller.js");
+
+  var router = require("express").Router();
+
+  // Create a new Article
+  router.post("/", articles.create);
+
+  // Retrieve all Articles
+  router.get("/", articles.findAll);
+
+  // Retrieve all published Articles
+  router.get("/published", articles.findAllPublished);
+
+  // Retrieve all not published Articles
+  router.get("/unpublished", articles.findAllNotPublished);
+
+  // Retrieve a single Article with id
+  router.get("/:id", articles.findOne);
+
+  // Publish Article by id
+  router.post("/publish/:id", articles.publish);
+
+  // Publish Article by id
+  router.post("/unpublish/:id", articles.unpublish);
+
+  // Update a Article with id
+  router.put("/:id", articles.update);
+
+  // Delete a Article with id
+  router.delete("/:id", articles.delete);
+
+  // Create a new Article
+  router.delete("/", articles.deleteAll);
+
+  app.use("/api/articles", router);
+};
